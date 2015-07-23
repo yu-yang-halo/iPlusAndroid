bc3323d34b2b1d753f902c85a5ae8cedf5c93142
yuyang <623240480@qq.com>
2015-07-23 18:12:27 +0800

    android 7/23 commit 启动画面功能添加

---
+++ app/src/main/java/cn/lztech/RegexUtils.java
@@ -0,0 +1,24 @@
+package cn.lztech;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Administrator on 2015/7/23.
+ */
+public class RegexUtils {
+    public  static  boolean isIPAddress(String ipaddr){
+        /**
+         * 判断IP格式和范围
+         */
+        String rexp = "((?:(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))";
+
+        Pattern pat = Pattern.compile(rexp);
+
+        Matcher mat = pat.matcher(ipaddr);
+
+
+        return mat.matches();
+
+    }
+}
